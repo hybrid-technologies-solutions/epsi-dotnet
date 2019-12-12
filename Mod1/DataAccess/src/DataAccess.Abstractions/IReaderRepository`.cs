aed21e7d40a81374205b21c2d4995ca229c20219
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2019-12-12 08:35:28 +0100

    add needed using for exercice

--- Mod1/DataAccess/src/DataAccess.Abstractions/IReaderRepository`.cs
+++ Mod1/DataAccess/src/DataAccess.Abstractions/IReaderRepository`.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace DataAccess.Abstractions
 {
     public interface IReaderRepository<T>
         where T : class
     {
+
     }
 }
